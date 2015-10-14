144e83cef92cd1bc609b0f14d0d1d17c249cb15a
Simon Støvring <simonstoevring@gmail.com>
2015-10-14 17:46:40 +0200

    Shows controllable objects

--- Cursor/Source/Pages/Location/LocationViewController.swift
+++ Cursor/Source/Pages/Location/LocationViewController.swift
@@ -13,6 +13,8 @@ class LocationViewController: UIViewController, ESTIndoorLocationManagerDelegate
     let indoorManager = ESTIndoorLocationManager()
     let location: ESTLocation
     
+    private var controllableDevices: [ControllableDevice] = []
+    
     var contentView: LocationView {
         return view as! LocationView
     }
@@ -41,15 +43,48 @@ class LocationViewController: UIViewController, ESTIndoorLocationManagerDelegate
         super.viewDidLoad()
         
         contentView.indoorLocationView.rotateOnPositionUpdate = true
+        contentView.indoorLocationView.drawLocation(location)
+        reloadControllableDevices()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         contentView.indoorLocationView.drawLocation(location)
+        redrawControllableDevices()
     }
     
     func indoorLocationManager(manager: ESTIndoorLocationManager!, didUpdatePosition position: ESTOrientedPoint!, withAccuracy positionAccuracy: ESTPositionAccuracy, inLocation location: ESTLocation!) {
         contentView.indoorLocationView.updatePosition(position)
+    }
+    
+    private func reloadControllableDevices() {
+        client?.devices { [weak self] result in
+            self?.removeControllableDevices()
+            self?.controllableDevices = result.value ?? []
+            self?.drawControllableDevices()
+        }
+    }
+    
+    private func redrawControllableDevices() {
+        removeControllableDevices()
+        drawControllableDevices()
+    }
+    
+    private func removeControllableDevices() {
+        controllableDevices.forEach {
+            self.contentView.indoorLocationView.removeObjectWithIdentifier(String($0.id))
+        }
+    }
+    
+    private func drawControllableDevices() {
+        controllableDevices.forEach {
+            let point = ESTOrientedPoint(x: 2, y: 2, orientation: 0)
+            let objectView = UIView(frame: CGRectMake(0, 0, 40, 40))
+            objectView.backgroundColor = .blueColor()
+            self.contentView.indoorLocationView.drawObjectInBackground(objectView,
+                withPosition: point,
+                identifier: String($0.id))
+        }
     }
 }
